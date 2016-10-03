41e52f333728b2723bb64f77e335a0cfd53c463a
DennisStud <dennis.scholl@stud.uni-goettingen.de>
2016-10-03 18:30:43 +0200

    Speisekarten aus Daten der Datenbank erstellen funktioniert!

--- view/speisekarte.php
+++ view/speisekarte.php
@@ -6,40 +6,68 @@ require 'templates/headblockview.php';
  * and open the template in the editor.
  */
 ?>
-
 <body style="background-image: url(img/holz.jpg)">
     <div class="row">
         <div class="col-md-3">
+            <div class="sidebar-nav-fixed affix">
+                <div class="well">
+                    <ul class="nav ">
+                        <li class="nav-header">Sidebar</li>
+                        <?php
+                        $kategorie = Kategorie::getAllKategorien();
+                        $anzahlkat = count($kategorie) - 1;
+
+
+                        for ($x = 0; $x <= $anzahlkat; $x++) {
+                            ?>
+
+                            <li><a href="index.php?action=meals&aktuellkategorieid=<?php echo $x ?>"><?php echo $kategorie[$x] ?></a></li></tr>  
+                        <?php } ?>
+                    </ul>
+                </div>
+                <!--/.well -->
+            </div>
+            <!--/sidebar-nav-fixed -->
+        </div>
+        <!--    <div class="row">-->
+        <div class="col-md-9">
+            <?php
+            ?>
+
             <table class="table">
                 <thead>
-                    <tr>
-                <th>Dies soll die navigationsleiste links werden</th></tr>
-                <?php
-                $kategorie = Kategorie::getAllKategorien();
-                $anzahl = count($kategorie) -1;
+                    <tr> 
+                        <th>lfd.Nr</th>
+                        <th>Gericht</th>
+                        <th>Beschreibung</th>
+                        <th>Preis</th>
+                        <th>Anzahl</th>
+                        <th>      </th>
+
+                </thead>
+                <tbody>
+                    </tr>
+                    <?php
+                    $speisen = Speisen::getSpeisen($aktuellkategorieid);
+                    $anzahlger = count($speisen) - 1;
 
 
-                for ($x = 0; $x <= $anzahl; $x++) {
-                    ?>
-                    <tr class="table-row" data-href="zeile">
-                        <td><?php echo $kategorie[$x]; ?></td></tr>  
-                <?php } ?>
-            </thead>
-            </table> 
+                    for ($x = 0; $x <= $anzahlger; $x++) {
+                        ?>
+                        <tr class="table-row">
+                            <?php for ($y = 0; $y <= 3; $y++) { ?>
+                                <td>
+                                    <?php echo $speisen[$x][$y] ?>
+                                </td>
+
+                            <?php } ?>
+                            <td><input class="number"></td>            
+                            <td><a href="index.php?action=bestellen" >bestellen</a></td>    
+                        </tr> 
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
-
-        <div class="col-md-9">
-            <table class="table">
-                <tr> 
-                    <th>lfd.Nr</th>
-                    <th>Gericht</th>
-                    <th>Beschreibung</th>
-                    <th>Preis</th> 
-                    <th>      </th>
-                   
-                </tr>
-                <td>        Hier soll eine Schleife eingebunden werden</td>
-                        </div>
 </body>
 
 <?php
